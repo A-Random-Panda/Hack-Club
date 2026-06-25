9a2f4246c8a3f8ad1b42ee5553b8dd5534ec059d
zhang-shuning <ningnongidk@gmail.com>
2026-06-24 18:45:38 -0700

    fixed bugs

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ if mode == "j":
 else:
     print("your ip is ", socket.gethostbyname(socket.gethostname()))
 
-#Setup
+#Create app
 app = Ursina()
+
+#Importing models
+grid = Entity(model=Grid(20,20), scale=50, color=color.white, rotation_x=90, y=1, collider ="box")
+yoru = Entity(model=Plane(subdivisions=[2,8]),scale= 50, color=color.white,texture="test123",rotation_x=0, y=0, collider = "box")
+player_shadow = Entity(model="PlayerModel", color=color.white,texture="Bamboo",rotation_x=0, y=0, enabled = False)
+
+#Setup
 camoverlay = Text (parent= camera.ui,scale=2,position=(-0.7,0.4),color=color.gray)
 camoverlay.disable()
 
@@ -35,18 +42,14 @@ player = FPC(
 )
 player.visible = False
 
-#Importing models
-grid = Entity(model=Grid(20,20), scale=50, color=color.white, rotation_x=90, y=1, collider ="box")
-yoru = Entity(model=Plane(subdivisions=[2,8]),scale= 50, color=color.white,texture="test123",rotation_x=0, y=0, collider = "box")
-player_shadow = Entity(model="PlayerModel", color=color.white,texture="Bamboo",rotation_x=0, y=0, enabled = False)
 
-# Cotrols
+# Controls
 class Controls():
     TOGGLE_CAMERA = 'c'
     RESET_CAMERAS = 'r'
     PLACE_CAMERA = 'left mouse down'
     FREECAM_MODE = 'p'
-    QUIT_GAME = 'escape'n
+    QUIT_GAME = 'escape'
 
 # Walls
 wall1 = Entity(model="cube", scale=(50,12,1), color=color.red, collider = "box", x=0, z=-24)
@@ -82,30 +85,11 @@ def input(key):
     global in_camera
     global camera_position_list
     global current_cam
-    """
-    if key == "f": # player
-        player.texture = "Bamboo.png"
-        player.model="PlayerModel.obj"
-        player.position = saved_pos
-        player.speed = 20
-        player.jump_height=4
-        player.gravity = 1
-        in_camera = False
-    """
+
     if key == Controls.TOGGLE_CAMERA: # camera
         current_cam += 1
         if current_cam == len(camera_position_list):
             current_cam = 0
-        '''
-        saved_pos = player_shadow.position
-        player.texture ="cam"
-        player.model = "cypher_cam"
-        player.position = (5,5,5)
-        player.speed = 0
-        player.jump_height=0
-        player.gravity = 0
-        in_camera = True
-        '''
 
         if current_cam == 0 and len(camera_position_list) != 1: #player
             player.texture = "Bamboo.png"
@@ -147,7 +131,7 @@ def input(key):
                                 color = color.orange,
                                 collider = 'box',
                                 position = hit.world_point,
-                                texture="in_camera",
+                                texture = "cam",
                                 rotation = (player.rotation[0]+180,player.rotation[1],player.rotation[2]+180)))
             camera_position_list.append(hit.world_point)
             pass
