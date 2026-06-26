d724dea32dfffa93f64514960b6d02268d788074
zhang-shuning <ningnongidk@gmail.com>
2026-06-26 02:16:33 -0700

    Started mapmaker (still WIP)

---
+++ mapmaker.py
@@ -0,0 +1,11 @@
+'''This hopefully will become a mapmaker for Ursina
+Currently very WIP while I learn the library
+'''
+
+import json
+from ursina import *
+from scripts.controls import Controls
+from scripts.editable_controls_FPC import FirstPersonControllera
+from scripts.mapmaker_constants import *
+
+app = Ursina()
