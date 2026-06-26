1e03386efcf4e1e57950251d3a6b302feed1cab8
zhang-shuning <ningnongidk@gmail.com>
2026-06-26 02:58:09 -0700

    Switchable EditorCamera/FirstPersonController in mapmaker

--- mapmaker.py
+++ mapmaker.py
@@ -3,9 +3,41 @@ Currently very WIP while I learn the library
 '''
 
 import json
+import logging
 from ursina import *
-from scripts.controls import Controls
-from scripts.editable_controls_FPC import FirstPersonControllera
+from scripts.player import _player_properties
+from scripts.controls import Controls, get_binding
+from scripts.editable_controls_FPC import FirstPersonController
 from scripts.mapmaker_constants import *
 
+#Declare logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#Create app
 app = Ursina()
+
+#Create grid
+grid = Entity(model=Grid(*GRID_SIZE), scale=50, color=color.white, rotation_x=90, y=1, collider ="box")
+
+#test for now
+yoru = Entity(model=Plane(subdivisions=[2,8]),scale= 50, color=color.white,texture="test123",rotation_x=0, y=0, collider = "box")
+
+fpc = FirstPersonController(**_player_properties)
+fpc.disable()
+
+editor_camera = EditorCamera()
+editor_camera.enable()
+
+
+def input(key):
+    logger.debug("key %s pressed", key)
+    ##Switch camera modes
+    if key == get_binding(Controls.FREECAM_MODE):
+        logger.info("Switch controls happenning")
+        editor_camera.enabled = not editor_camera.enabled
+        fpc.enabled = not fpc.enabled
+        logger.info("Editor camera state: %s", editor_camera.enabled)
+        logger.info("fpc state: %s", fpc.enabled)
+
+app.run()
