d8a9c1d8fab943dd30d78c26656fa3ce34c277ae
zhang-shuning <ningnongidk@gmail.com>
2026-07-15 02:27:31 -0700

    Adds date and level to server logging.

--- multiplayer_test/multiplayer_test.py
+++ multiplayer_test/multiplayer_test.py
@@ -10,12 +10,19 @@ Decide on which method of communication is the best:
 The current plan is to just kinda have everything be a server, and running it locally just means hosting it on the computer
 '''
 
+import argparse
 import logging
 from typing import override
 from collections import deque
 
 from ursina import *
 from ursina.networking import *
+
+#Declare command line arguments
+parser = argparse.ArgumentParser(description="The script to start the game server.")
+parser.add_argument("-host", "--hostname", type=str, default="localhost", help="The hostname for the server.")
+parser.add_argument("-p", "---port", type=int, default=1939, help="The port the server is hosted on.")
+args = parser.parse_args()
 
 #Declare Ursina app
 app = Ursina(borderless=False)
@@ -25,25 +32,35 @@ class LatestLogHandler(logging.Handler):
     '''Handler that puts the latest log into a variable'''
     MAX_LOGS:int = 30
     latest_logs:deque[str] = deque(["Default message"])
-    
+
     @classmethod
-    def save_log(cls, record: logging.LogRecord) -> None:
+    def save_log(cls, formatted_record: str) -> None:
         '''Saves the log of the message'''
         if len(cls.latest_logs) >= cls.MAX_LOGS:
             cls.latest_logs.popleft()
-        cls.latest_logs.append(record.getMessage())
+        cls.latest_logs.append(formatted_record)
 
     @classmethod
     def get_logs(cls) -> str:
+        '''Returns the last MAX_LOGS logs'''
         return "\n".join(cls.latest_logs)
 
     @override
     def emit(self, record: logging.LogRecord) -> None:
-        self.save_log(record)
+        self.save_log(self.format(record))
+
+handler = LatestLogHandler()
+
+formatter = logging.Formatter(
+    fmt="(%(asctime)s) %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+handler.setFormatter(formatter)
 
 logging.basicConfig(level=logging.INFO)
 logger:logging.Logger = logging.getLogger(__name__)
-logger.addHandler(LatestLogHandler())
+logger.addHandler(handler)
 
 #Declare server and server variables
 server:RPCPeer = RPCPeer()
@@ -53,11 +70,9 @@ connection_count:int = 0
 START_TEXT:str = "The server is not on"
 status_text:Text = Text(text=START_TEXT, origin=(0, 0))
 
-
-'''Server class'''
 def start_server(hostname, port):
     '''The function that starts the server'''
-    logger.info("The server started.")
+    logger.info("Starting server on port %d.", port)
     server.start(hostname, port, is_host=True)
 
 def update():
@@ -72,8 +87,7 @@ def input(key):
         logger.warning("Test warning")
 
 if __name__ == "__main__":
-    logger.info("Ran from main")
-    start_server("localhost", 1939)
-
-app.run()
-
+    start_server(args.hostname, args.port)
+    app.run()
+else:
+    logger.fatal("Server was not properly run; please run the server directly!.")
