1bc37f79f76762276723fb23e07f4f346d964734
A-Random-Panda <yaoh667@gmail.com>
2026-07-16 02:54:00 -0700

    Moved most of the UI elements to a seperate file and instead of calling indiviual functions it's now a class

--- scripts/Minimap.py
+++ scripts/Minimap.py
@@ -13,7 +13,7 @@ class MinimapIcons:
     def __init__(self):
         self.minimap:Entity = Entity(scale=(0.2,0.2), x = 0.7, y= 0.4, model = "quad", texture="map_1", parent = camera.ui)
         self.player_icon:Entity = Entity(parent = self.minimap, texture = "red_dot", scale = 0.05, model = "quad", z =-0.5, color=color.red)
-        self.sqaure_icon = Entity(parent = self.minimap, scale = 0.05, model = "quad", z =-0.5)
+        self.square_icon = Entity(parent = self.minimap, scale = 0.05, model = "quad", z =-0.5)
         self.vision_cone_icon = Entity(scale = (4,0.2), parent= self.player_icon, model = "quad", z = -1, color = color.red, origin = (0.7,0), a = 0.4 )
 
 
