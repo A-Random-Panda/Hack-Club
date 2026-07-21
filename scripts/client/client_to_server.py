75d5e2413b9c2342311278aceb071bee3a5529b4
zhang-shuning <ningnongidk@gmail.com>
2026-07-21 01:48:32 -0700

    rewrote the parsing in python, made seperate folder for client

---
+++ scripts/client/client_to_server.py
@@ -0,0 +1,43 @@
+from ursina import *
+from scripts.in_game.player import _Player
+def send_info(player:_Player):
+    cam_position_list:list[Vec3] = []
+    cam_rotation_list:list[Vec3] = []
+    for cam in player.perspective_list:
+        cam_position_list.append(cam.world_position)
+        cam_rotation_list.append(cam.rotation)
+
+    if player.bullet_trail is not None and player.bullet_trail:
+        return f'''{len(player.perspective_list)}
+{"\n".join([str(tuple(x)) for x in cam_position_list])}
+{"\n".join([str(tuple(x)) for x in cam_rotation_list])}
+{True}
+{player.reload_time}
+{player.dead}
+{tuple(player.world_position)}
+{tuple(player.rotation)}
+{player.points}
+{player.in_zone}'''
+    return f'''{len(player.perspective_list)}
+{"\n".join([str(tuple(x)) for x in cam_position_list])}
+{"\n".join([str(tuple(x)) for x in cam_rotation_list])}
+{False}
+{player.reload_time}
+{player.dead}
+{tuple(player.world_position)}
+{tuple(player.rotation)}
+{player.points}
+{player.in_zone}'''
+
+def info_key():
+    return  ("amount of player cams",
+               "player cams position: list[vec3]", 
+               "player cams rotation: list[vec3]", 
+               "shooting bullet: bool"
+               "reload time: float",
+               "player dead: bool",
+               "player world position: vec3",
+               "player rotation: vec3",
+               "player points: boolean",
+               "player in zone: boolean"
+               )
---
+++ scripts/client/parsing.py
@@ -0,0 +1,57 @@
+'''Parser for the client'''
+import logging
+from ast import literal_eval
+
+_logger = logging.getLogger(__name__)
+
+def parse_state(string:str):
+    '''
+    I give up on the c implementation for now, just writing something fast in python for now
+    Ideally this would be after serverside verification but uh, not happenning as of right now
+    '''
+    return_dict = {}
+    position_list:list[tuple[float]] = []
+    rotation_list:list[tuple[float]] = []
+    try:
+        #Index to traverse through list
+        index:int = 1
+        #Amount of cameras
+        state_list:list[str] = string.split("\n")
+        camera_amount:int = int(state_list[0])
+        return_dict["cam_amount"] = camera_amount
+        #Positions
+        for _ in range(camera_amount):
+            position_list.append(literal_eval(state_list[index]))
+            index+=1
+        return_dict["positions"] = position_list 
+        #Rotation
+        for _ in range(camera_amount):
+            rotation_list.append(literal_eval(state_list[index]))
+            index+=1
+        return_dict["rotations"] = rotation_list
+        #Whether player is shooting
+        return_dict["is_shooting"] = bool(state_list[index])
+        index+=1
+        #reload time
+        return_dict["reload_time"] = float(state_list[index])
+        index+=1
+        #Whether it's dead
+        return_dict["is_dead"] = bool(state_list[index])
+        index+=1
+        #World position
+        return_dict["world_pos"] = literal_eval(state_list[index])
+        index+=1
+        #Player rotation
+        return_dict["player_rotation"] = literal_eval(state_list[index])
+        index+=1
+        #Player points
+        return_dict["points"] = int(state_list[index])
+        index +=1
+        #In zone
+        return_dict["in_zone"] = bool(state_list[index])
+
+    except IndexError as err:
+        _logger.error("%s: String inputted %s is not correct", err, string)
+    except ValueError as err:
+        _logger.error("%s: Something went wrong with text parsing", err)
+    return return_dict
