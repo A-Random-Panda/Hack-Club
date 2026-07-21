fa3244b3e13e5d3517e95feb303691ede44dfc94
A-Random-Panda <yaoh667@gmail.com>
2026-07-21 01:45:58 -0700

    working on a function to start the game

--- scripts/in_game/start_game.py
+++ scripts/in_game/start_game.py
@@ -1,3 +1,16 @@
+from ursina import *
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from scripts.in_game.player import _Player
+    from scripts.in_game.audio_controller import AudioController
+    from scripts.in_game.ui import UIController
+def start_game(self,player:"_Player",audio:"AudioController",ui:"UIController"):
+    player.position = player.respawn_point
+    player.cash = 1000
+    ui.close_all_uis()
+    player.reload_time = 5
+    player.points = 0
+    player.max_cams = 5
+    player.reload_time = 5
 
-def start_game():
     pass
