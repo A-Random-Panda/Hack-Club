50be65e4480b971c8c7ec4093d8b5fe5e96b1f37
zhang-shuning <ningnongidk@gmail.com>
2026-07-23 03:57:39 -0700

    Finished purgatory map, changed asset folder stucture.

--- maps/map_purgatory.py
+++ maps/map_purgatory.py
@@ -1,18 +1,59 @@
 '''This is the first map made, future maps may or may not be in JSON format'''
 
-#This won't be in the main i
 from ursina import *
 
 class Purgatory():
+    '''Helper class for the purgatory class'''
     map_loaded = False
     entity_list:list[Entity] = []
 
     @classmethod
     def load_map(cls):
-        '''How this will work is that static entities won't have a name while other entities will.'''
+        '''
+        Method to load the map.
+        Eventually, it probably is better to put split objects together
+        '''
+        if cls.map_loaded:
+            return
         cls.map_loaded = True
         cls.entity_list = [
-        Entity(model=Plane(subdivisions=[2,8]),scale= 100, texture='test123', color=color.white, collider = "box") #floor
+        #Floor
+        Entity(model="cube",scale=(60, 1, 40), texture='purg_floor', color=color.white, collider="box"),
+
+        #Roof
+        Entity(model="cube",scale=(60, 1, 40), y = 20, texture='purg_roof', color=color.white, collider="box"),
+
+        #Walls across x axis
+        Entity(model="cube", scale=(50, 20, 1), position=(0, 10, 20), texture='purg_bound', color=color.white, collider="box"),
+        Entity(model="cube", scale=(50, 20, 1), position=(0, 10, -20), texture='purg_bound', color=color.white, collider="box"),
+        Entity(model="cube", scale=(5, 20, 1), position=(27.5, 10, 2.5), texture='purg_bound', color=color.white, collider="box"),
+        Entity(model="cube", scale=(5, 20, 1), position=(27.5, 10, -2.5), texture='purg_bound', color=color.white, collider="box"),
+        Entity(model="cube", scale=(5, 20, 1), position=(-27.5, 10, 2.5), texture='purg_bound', color=color.white, collider="box"),
+        Entity(model="cube", scale=(5, 20, 1), position=(-27.5, 10, -2.5), texture='purg_bound', color=color.white, collider="box"),
+
+        #Walls across z axis
+        Entity(model="cube", scale= (1, 20, 17.5), position = (25, 10, 11.25), texture='purg_bound', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 20, 17.5), position = (25, 10, -11.25), texture='purg_bound', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 20, 17.5), position = (-25, 10, 11.25), texture='purg_bound', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 20, 17.5), position = (-25, 10, -11.25), texture='purg_bound', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 20, 5), position = (30, 10, 0), texture='purg_bound', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 20, 5), position = (-30, 10, 0), texture='purg_bound', color=color.white, collider = "box"),
+
+        #near spawn walls
+        Entity(model="cube", scale= (1, 10, 15), position = (20, 5, 0), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 10, 15), position = (-20, 5, 0), texture='purg_wall', color=color.white, collider = "box"),
+
+        #Closer walls across z axis
+        Entity(model="cube", scale= (1, 10, 10), position = (15, 5, 10), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 10, 10), position = (-15, 5, 10), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 10, 10), position = (15, 5, -10), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (1, 10, 10), position = (-15, 5, -10), texture='purg_wall', color=color.white, collider = "box"),
+
+        #Closer walls across x axis point walls
+        Entity(model="cube", scale= (12.5, 10, 1), position = (8.75, 5, 15), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (12.5, 10, 1), position = (-8.75, 5, 15), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (12.5, 10, 1), position = (8.75, 5, -15), texture='purg_wall', color=color.white, collider = "box"),
+        Entity(model="cube", scale= (12.5, 10, 1), position = (-8.75, 5, -15), texture='purg_wall', color=color.white, collider = "box")
         ]
 
     @classmethod
@@ -27,10 +68,16 @@ class Purgatory():
 
 if __name__ == "__main__":
     from ursina.prefabs.first_person_controller import FirstPersonController
+    from ursina.prefabs.editor_camera import EditorCamera
     from sys import exit
 
     application.asset_folder = Path(r"../assets")
     app = Ursina()
+
+    coordinate_text = Text(scale=1, position = (-.5*1.778, .5), origin = (-.5, .5))
+
+    player_shadow = Entity(model="Better_Tank", color=color.red,scale = 0.5, collider="mesh", enabled=False)
+    player_line = Entity(model = Quad(radius=200, mode='line'), color=color.white, enabled=False)
 
     fpc = FirstPersonController(
         speed = 16,
@@ -38,6 +85,8 @@ if __name__ == "__main__":
         collider="box",
         gravity= 1,
         enabled = True)
+
+    editor_camera = EditorCamera(enabled=False, move_speed=50)
 
     Purgatory.load_map()
 
@@ -48,9 +97,33 @@ if __name__ == "__main__":
             Purgatory.unload_map()
         if key == "2":
             Purgatory.load_map()
+        if key == 'tab':
+            if fpc.enabled:
+                fpc.disable()
+                player_shadow.enable()
+                player_line.enable()
+                editor_camera.position = fpc.position
+                editor_camera.enable()
+            else:
+                editor_camera.disable()
+                player_shadow.disable()
+                player_line.disable()
+                fpc.position = editor_camera.position
+                fpc.enable()
+        if key == "shift":
+            fpc.speed = 8
+        if key == "shift up":
+            fpc.speed = 16
 
     def update():
         Purgatory.update()
+        if editor_camera.enabled:
+            player_line.position = editor_camera.position
+            player_shadow.position = editor_camera.position
+            player_line.rotation = editor_camera.rotation
+            coordinate_text.text = editor_camera.position
+        else:
+            coordinate_text.text = fpc.position
         if fpc.y_getter() < -10:
             fpc.position_setter((0, 10, 0))
 
