19d427af405e73e0a697a73479c815d267e69a55
zhang-shuning <ningnongidk@gmail.com>
2026-07-22 17:12:44 -0700

    Now sends you into the game after the lobby

--- scripts/client/rpc_functions.py
+++ scripts/client/rpc_functions.py
@@ -9,6 +9,7 @@ from ursina.networking import *
 
 if TYPE_CHECKING:
     from scripts.game.ui import UIController
+    from scripts.game.main_menu import MainMenu
 
 _logger = getLogger(__name__)
 peer:RPCPeer = RPCPeer()
@@ -18,11 +19,17 @@ class GameState():
     game_started = False
     state_string:str = ""
     game_state:dict[str, Any] = {}
+
     ui_controller:"UIController | None" = None
+    main_menu:"MainMenu | None" = None
     @classmethod
     def set_ui_controller(cls, controller:"UIController"):
         '''Sets the ui controller, meant to be used in initialization'''
         cls.ui_controller = controller
+    @classmethod
+    def set_main_menu(cls, menu:"MainMenu"):
+        '''Sets the ui controller, meant to be used in initialization'''
+        cls.main_menu = menu
 
 @rpc(peer)
 def state_to_client(connection, time_received, state:str):
@@ -38,7 +45,10 @@ def names_to_client(connection, time_received, name:str):
 @rpc(peer)
 def game_started_state(connection, time_received, gamestate:bool):
     '''Controls whether the game has started or not'''
+    assert GameState.main_menu is not None
     GameState.game_started = gamestate
+    GameState.main_menu.exit_subscreen()
+    GameState.main_menu.enter_game()
 
 @rpc(peer)
 def on_connect(connection, time_connected):
