a0c51d5db6743b62fdd74ed39ae20343b010b070
A-Random-Panda <yaoh667@gmail.com>
2026-07-30 21:29:38 -0700

    fixed a leaderboard bug

--- main.py
+++ main.py
@@ -458,13 +458,15 @@ def update():
                     player.laser.disable()
                     cam_switching(True)
                     end_round(player,ui_controller, state["points"])
-                    if player.round_wins == 7 and not player.game_over:
+                    ui_controller.enemy_leaderboard_text.text = f"{state["points"]} {GameState.opponent_name.replace(player.username, "")} \n round wins: {state["round_wins"]}"
+                    ui_controller.leaderboard_text.text = f"{player.points} {player.username} \n round wins: {player.round_wins}"
+                    if player.round_wins == 2 and not player.game_over:
                         ui_controller.menu_overlay.enable()
                         ui_controller.game_win.enable()
                         invoke(reset_values, player, ui_controller, state["opponent_id"], GameState.id, delay = 10)
                         invoke(set_spawn, player, state["opponent_id"], GameState.id, delay = 10)
                         player.game_over = True
-                    elif state["round_wins"] == 7 and not player.game_over:
+                    elif state["round_wins"] == 2 and not player.game_over:
                         ui_controller.menu_overlay.enable()
                         ui_controller.game_lose.enable()
                         invoke(reset_values, player, ui_controller, state["opponent_id"], GameState.id, delay = 10)
