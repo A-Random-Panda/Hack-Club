5a508f5f243e886793940b0d5764c219a6559ee3
zhang-shuning <ningnongidk@gmail.com>
2026-07-30 21:44:58 -0700

    reset values 2 positional arguments

--- main.py
+++ main.py
@@ -463,13 +463,13 @@ def update():
                     if player.round_wins == 2 and not player.game_over:
                         ui_controller.menu_overlay.enable()
                         ui_controller.game_win.enable()
-                        invoke(reset_values, player, ui_controller, state["opponent_id"], GameState.id, delay = 10)
+                        invoke(reset_values, player, ui_controller, state["opponent_id"])
                         invoke(set_spawn, player, state["opponent_id"], GameState.id, delay = 10)
                         player.game_over = True
                     elif state["round_wins"] == 2 and not player.game_over:
                         ui_controller.menu_overlay.enable()
                         ui_controller.game_lose.enable()
-                        invoke(reset_values, player, ui_controller, state["opponent_id"], GameState.id, delay = 10)
+                        invoke(reset_values, player, ui_controller, state["opponent_id"])
                         invoke(set_spawn, player, state["opponent_id"], GameState.id, delay = 10)
                         player.game_over = True
 
